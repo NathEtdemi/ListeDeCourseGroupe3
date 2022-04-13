563d8e8e60dfcf663c238ed7cf47ff3679827bf7
Nathim <nathimrichar@gmail.com>
2022-04-13 20:53:35 +0200

    Design Image Boutton (add , suppr) + barre de navigation

--- app/src/main/java/com/example/listedecoursegroupe3/MainActivity.java
+++ app/src/main/java/com/example/listedecoursegroupe3/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.listedecoursegroupe3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.ImageButton;
 
 import com.example.listedecoursegroupe3.entites.Produit;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.j256.ormlite.dao.Dao;
 
 import java.sql.SQLException;
@@ -23,13 +29,41 @@ public class MainActivity extends AppCompatActivity
     private Button Liste;
     private Button AddProduit;
     private Button Recette;
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    AjoutProduitFragment ajoutProduitFragment = new AjoutProduitFragment();
+    RecetteFragment recetteFragment = new RecetteFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.liste:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.AjoutProduit:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, ajoutProduitFragment).commit();
+                        return true;
+                    case R.id.Receipt:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, recetteFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
         grille=findViewById(R.id.layout);
-        Liste = findViewById(R.id.liste);
+        Liste = findViewById(R.id.listeProduit);
         AddProduit = findViewById(R.id.Add_Product);
         Recette = findViewById(R.id.Recette);
         DataBaseLinker linker = new DataBaseLinker(this);
@@ -39,11 +73,19 @@ public class MainActivity extends AppCompatActivity
             List<Produit> produit = daoProduit.queryForAll();
             for(Produit Produits: produit)
             {
+                TableRow.LayoutParams paramButton = new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.WRAP_CONTENT,
+                        1f
+                );
+
                 TableRow newRow = new TableRow(this);
                 TextView newText = new TextView(this);
                 newText.setText(Produits.getLibelleProduit());
-                Button Modifier = new Button(this);
-                Modifier.setText("Modifier Produit");
+                ImageButton Modifier = new ImageButton(this);
+                Modifier.setLayoutParams(paramButton);
+                Modifier.setBackground(null);
+                Modifier.setImageResource(R.drawable.edit);
                 Modifier.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v)
@@ -53,6 +95,24 @@ public class MainActivity extends AppCompatActivity
                         startActivity(monIntent);
                     }
                 });
+
+                ImageButton SupprimerProduit = new ImageButton(this);
+                SupprimerProduit.setLayoutParams(paramButton);
+                SupprimerProduit.setImageResource(R.drawable.delete);
+                SupprimerProduit.setBackground(null);
+                SupprimerProduit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            daoProduit.delete(Produits);
+                        }
+                        catch (SQLException throwables) {
+                            throwables.printStackTrace();
+                        }
+                        ((ViewGroup) newRow.getParent()).removeView(newRow);
+                    }
+                });
+
                 Button Plus = new Button(this);
                 TextView quantité = new TextView(this);
                 quantité.setText("1");
@@ -64,6 +124,7 @@ public class MainActivity extends AppCompatActivity
                 newRow.addView(quantité);
                 newRow.addView(Plus);
                 newRow.addView(Modifier);
+                newRow.addView(SupprimerProduit);
                 grille.addView(newRow);
                 Plus.setOnClickListener(new View.OnClickListener() {
                     @Override
